9ee67b438103b22cca0c5f2428ab15ea7761e157
Shivam Pandey <shivamguys@gmail.com>
2020-06-21 19:40:19 +0530

    instruction

--- static_content.py
+++ static_content.py
@@ -2,7 +2,10 @@
 
 from flask import Blueprint,request,send_file,make_response,redirect,Response,render_template
 static_content=Blueprint('serving this as static content api', __name__, template_folder='templates')
-
+"""
+You can use this route as some static content used in function instead of html file
+Frequent modification of dynamic server side content can be used here.
+"""
 
 
 
