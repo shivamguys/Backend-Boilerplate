5d934b71a3a8bde808070235a175b127da01df59
Shivam Pandey <shivamguys@gmail.com>
2020-06-21 19:05:55 +0530

    environment instruction

--- sqlhost.py
+++ sqlhost.py
@@ -1,4 +1,9 @@
 import os
+"""
+Configure your secure connection variable here, to be used with microservices
+
+"""
+
 host=os.environ.get('db_host')
 port=os.environ.get('db_port')
 username=os.environ.get('db_username')
